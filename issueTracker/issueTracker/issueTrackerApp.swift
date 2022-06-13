d9a00ea65ce604b61214e6413f4f9af3cc703987
shingha <shingha1124@gmail.com>
2022-06-13 16:46:31 +0900

    프로젝트 초기 설정

---
+++ issueTracker/issueTracker/issueTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  issueTrackerApp.swift
+//  issueTracker
+//
+//  Created by seongha shin on 2022/06/13.
+//
+
+import SwiftUI
+
+@main
+struct issueTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

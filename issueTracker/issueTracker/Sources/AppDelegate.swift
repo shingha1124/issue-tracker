955d675513ca997e92230be985de7da3c05b652c
shingha <shingha1124@gmail.com>
2022-06-13 22:07:28 +0900

    프로젝트 초기 설정

---
+++ issueTracker/issueTracker/Sources/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  issueTracker
+//
+//  Created by seongha shin on 2022/06/13.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        true
+    }
+}
